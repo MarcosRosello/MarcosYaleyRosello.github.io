92d816e4a3dc991dcab6baaa39883e330569f248
MarcosRosello <55287902+MarcosRosello@users.noreply.github.com>
2020-02-03 10:46:01 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -1,14 +1,22 @@
-
-function myLoop1(){
-  var start = document.getElementById("start").value;
-  
-  var end = document.getElementById("end").value;
-  
-  for(var i=start; i<=end; i++){
-    document.write('<p>'+i);
-  }
-  for(var i=start; i>=end; i--){
-    document.write('<p>'+i);
-  }
+function toSet(){
+  var X1 = document.getElementById("X1").value;
+  var Y1 = document.getElementById("Y1").value;
+  var X2 = document.getElementById("X2").value;
+  var Y2 = document.getElementById("Y2").value;
+  X1 = Number(X1);
+  Y1 = Number(Y1);
+  X2 = Number(X2);
+  Y2 = Number(Y2);
+  document.getElementById("orderPair1").innerHTML= "(" + X1 + " , " + Y1 +" )";
+  document.getElementById("orderPair2").innerHTML= "(" + X2 + " , " + Y2 +" )";
+}
+function toStart(){
+  document.getElementById("orderPair1").innerHTML= "(" +  " , "  +" )";
+  document.getElementById("orderPair2").innerHTML= "(" +  " , "  +" )";
+}
+function toFixed(){
+  toSet();
+  var distance = Math.sqrt(Math.pow(Math.abs(X2-X1),2)+Math.pow(Math.abs(Y2-Y1)));
+  distance = Math.round(distance*100)/100;
 
 }
